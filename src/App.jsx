d414306ee30da3248d3f4ea28e1d0f9497cffbe3
adrianohebreu <adriano12144@icloud.com>
2021-05-28 23:11:40 +0100

    aula 28052021

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Card from './Card.jsx';
-import clientes from './clientes';
+import { BrowserRouter , Route } from 'react-router-dom';
 
-function createCard(cliente) {
-    return <Card 
-    key={cliente.id}
-    codigo={cliente.id}
-    nome={cliente.name}
-    email={cliente.email}
-    fone={cliente.fone}/>
-}
+/** Paginas */
+
 
 function App() {
-    return <div>
-            <Header texto="99 CRM"/>
-            {clientes.map(createCard)}
-            <Footer/>
-           </div>;
+    return <BrowserRouter>
+            <Route exact="/" component="">
+
+            </Route>
+           </BrowserRouter>;
 }
 
 export default App;
---
+++ src/site/site.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Site () {
+    return <div>
+
+    </div>
+}
+
+export default Site;
