0d52af0dbc03c9ba96c61813127c0cff40f98d22
adrianohebreu <adriano12144@icloud.com>
2021-05-29 01:19:59 +0100

    Finalizado Aula 07

--- src/site/site.jsx
+++ src/site/site.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Menu from './components/menu';
+import Banner from './components/banner';
+import Features from './components/features';
+import Testemunho from './components/testemunho';
+import Precos from './components/precos';
+import Footer from './components/footer';
 
 function Site () {
     return <div>
-           <h1>Site</h1>
+           <Menu/>
+           <Banner/>
+           <Features/>
+           <Testemunho/>
+           <Precos/>
+           <Footer/>
            </div>
 }
 
